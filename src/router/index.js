902ec1e256c556096c0aad0722b45141eeafb200
matheusdamaiadev <matheusdamaia1@gmail.com>
2026-04-15 10:20:52 -0300

    html login e cadastro feito

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import LoginView from '../views/LoginView.vue'
+import CadastroView from '../views/CadastroView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/cadastro',
+    name: 'cadastro',
+    component: CadastroView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/CadastroView.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import logo from '../assets/logo.png'
+
+const router = useRouter()
+
+function irParaLogin() {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div class="container">
+    <!-- LADO ESQUERDO -->
+    <div class="left">
+      <!-- LOGO -->
+      <img :src="logo" alt="logo" class="logo" />
+
+      <div class="left-content">
+        <h1>Já tem uma conta?</h1>
+        <p>Faça Login.</p>
+
+        <button class="btn-login-left" @click="irParaLogin">
+          Login
+        </button>
+      </div>
+    </div>
+
+    <!-- LADO DIREITO -->
+    <div class="right">
+      <h1>Crie sua conta!</h1>
+      <p>Preencha seus dados</p>
+
+      <input type="text" placeholder="Nome" />
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Senha" />
+      <input type="password" placeholder="Confirmar Senha" />
+
+      <button class="btn-cadastrar">Cadastrar</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
