0d25e71c0fcdeac68a58399a93bdbcd1deb49502
wtfemanu <os9777190@gmail.com>
2026-05-04 11:51:56 -0300

    FEAT#4: da de ver os usuarios agora

---
+++ src/views/HubAdminView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {useRouter} from 'vue-router' 
+import LightButton from '../components/LightButton.vue';
+
+const router = useRouter()
+
+function verFretes() {
+    router.push('/fretesadm')
+}
+function verUsuarios() {
+    router.push('/usuarioslist')
+}
+</script>
+<template>
+<LightButton @click="verFretes" label="Fretes" ></LightButton>
+<LightButton @click="verUsuarios" label="Usuarios"></LightButton>
+</template>
