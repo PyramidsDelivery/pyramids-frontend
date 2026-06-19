c305274f522ee0f63bf19bc4ab92d7a8bda38eb1
wtfemanu <os9777190@gmail.com>
2026-06-19 15:10:27 -0300

    trocando servi2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://costasbrabo.class.fabricadesoftware.ifc.edu.br', // URL do seu Django
+  baseURL: 'https://costasbrabo.class.fabricadesoftware.ifc.edu.br/api', // URL do seu Django
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
